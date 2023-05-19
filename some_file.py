dcf1967abd53a4bef9fe6559a372532b1173465a
Timoshenko Artem <arttim1019@gmail.com>
2023-05-19 18:00:31 +0300

    work with API 1

--- some_file.py
+++ some_file.py
@@ -1,3 +1,6 @@
-print("Hello, I am skyber_2's first Bot!")
-print('HI')
-print('YES')
+# API
+import requests
+
+url_api = 'http://numbersapi.com/43'
+response = requests.get(url_api)
+print(response.text if response.status_code == 200 else response.status_code)
