1397be1ae5cba09a3af9c1e0840c84ffe43e0bb1
Timoshenko Artem <arttim1019@gmail.com>
2023-05-20 11:25:20 +0300

    Long polling

--- some_file.py
+++ some_file.py
@@ -2,9 +2,13 @@ import requests
 import time
 
 api_url = 'https://api.telegram.org/bot'
-bot_token = '5971044342:AAGvKEGRBjv8aBpUypGBFbckjXN25oYWQhU'
+
+with open('ideas_for_bot.txt', encoding='utf-8') as file:
+    bot_token = ''.join(list(file.readline())[-47:]).strip()
+
 counter = 0
 offset = -2
+timeout: int = 10
 
 
 def do_something() -> None:
@@ -13,13 +17,12 @@ def do_something() -> None:
 
 while True:
     start_time = time.time()
-    updates = requests.get(f'{api_url}{bot_token}/getUpdates?offset={offset + 1}').json()
+    updates = requests.get(f'{api_url}{bot_token}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+    print(updates)
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             do_something()
-
-    time.sleep(3)
     end_time = time.time()
     print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
