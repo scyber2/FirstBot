2e050bf0c4f240557eb8b200bfa8053d093c68c5
Timoshenko Artem <arttim1019@gmail.com>
2023-05-22 22:07:40 +0300

    Full complete an echo bot

--- FirstBot.py
+++ FirstBot.py
@@ -1,32 +1,50 @@
 # Эхо бот (сообщения которые ты отпаравляешь в бота, приходят тебе от него)
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
 with open('ideas_for_bot.txt') as file:
     bot_token: str = ''.join(list(file.readline())[-47:]).strip()
 
+api_url: str = 'https://api.telegram.org/bot'
+
 bot: Bot = Bot(token=bot_token)
-dispat: Dispatcher = Dispatcher()
+dp: Dispatcher = Dispatcher()
+
 
 # Этот хендлер будет срабатывать на команду /start
-@dispat.message(Command(commands=['start']))
 async def process_start(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот.\nЧто я делаю, можешь посмотреть, нажав на команду /help')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dispat.message(Command(commands=['help']))
 async def process_help(message: Message):
     await message.answer('Инструкция: то, что ты мне отправишь, я отправлю тебе)))')
 
 
+# Этот хендлер будет срабатывать на отправку боту фото
+async def send_photo(message: Message):
+    print(message.photo)
+    await message.reply_photo(message.photo[0].file_id)
+
+
+# Этот хендлер будет срабатывать на отправку боту аудио
+async def send_audio(message: Message):
+    await message.reply_voice(message.voice.file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dispat.message()
-async def send_message(message: Message):
+async def send_text(message: Message):
     await message.reply(text=message.text)
 
 
+dp.message.register(process_start, Command(commands=['start']))
+dp.message.register(process_help, Command(commands=['help']))
+dp.message.register(send_photo, F.photo)
+dp.message.register(send_audio, F.voice)
+dp.message.register(send_text)
+
 if __name__ == '__main__':
-    dispat.run_polling(bot)
+    dp.run_polling(bot)
+
